31ff08e8824d7436f1cfe65570743cbaa8f3232f
Mark <mamg7leon@gmail.com>
2026-01-08 00:29:40 -0600

    MASONITE FIX: Patches for Render Python 3.13

---
+++ masonite_fix.py
@@ -0,0 +1,13 @@
+﻿import sys, datetime
+class FixedPendulum:
+    @staticmethod
+    def now(): return datetime.datetime.now()
+    @staticmethod  
+    def today(): return datetime.date.today()
+    def __init__(self, *args, **kwargs): pass
+pendulum_module = type(sys)('pendulum')
+pendulum_module.now = FixedPendulum.now
+pendulum_module.today = FixedPendulum.today  
+pendulum_module.Pendulum = FixedPendulum
+pendulum_module.__version__ = '2.1.2'
+sys.modules['pendulum'] = pendulum_module
