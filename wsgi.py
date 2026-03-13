7a548710a4a40eed4914d9bb280ec70ff37188a8
Mark <mamg7leon@gmail.com>
2026-03-12 18:54:52 -0600

    ADD: Flechas paginado

--- wsgi.py
+++ wsgi.py
@@ -15,7 +15,7 @@ import os
 # CONFIG
 # =========================================================
 # Usamos directamente la URL de la base de datos proporcionada
-DB_URL = os.getenv('DB_URL', 'mysql://root:mxvHDOGWiQGekUUTxIFAXnIpmRlHnFZu@nozomi.proxy.rlyw.net:28752/railway')
+DB_URL = os.getenv('DB_URL', 'mysql://root:mxvHDOGWiQGekUUTxIFAXnIpmRlHnFZu@mysql.railway.internal:3306/railway')
 
 RECAPTCHA_SITE_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 RECAPTCHA_SECRET_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
