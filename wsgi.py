4859c0054d0f816531c8fe900521a66a4d3ad624
Mark <mamg7leon@gmail.com>
2026-03-25 23:20:56 -0600

    ADD: Flechas paginado

--- wsgi.py
+++ wsgi.py
@@ -267,6 +267,7 @@ def application(environ, start_response):
             <h2>👥 Gestión de Usuarios</h2>
             <div style='display:flex; justify-content:space-between; margin-bottom:15px; align-items:center;'>
                 <button class='btn-emerald' style='width:auto' onclick="openM('mNew')">+ NUEVO USUARIO</button>
+                # Busca esta línea y asegúrate de que esté limpia:
                 <input type="text" id="txtBusca" onkeyup="paginaActual=1; filtrar('.u-row', '.u-name');" placeholder="🔍 Buscar..." style="width:200px; margin:0;">
             </div>
             <table>
